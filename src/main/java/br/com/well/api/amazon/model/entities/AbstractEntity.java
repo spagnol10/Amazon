1e0ae625678e2d01560a7bb79120522e3fa6b162
spagnol10 <wellyntonspagnol10@gmail.com>
2023-07-15 10:47:56 -0300

    Delet files

--- src/main/java/br/com/well/api/amazon/model/entities/AbstractEntity.java
+++
@@ -1,7 +0,0 @@
-package br.com.well.api.amazon.model.entities;
-
-import java.io.Serializable;
-
-public abstract class AbstractEntity implements Serializable {
-    private static final long serialVersionUID = 1L;
-}
--- src/main/java/br/com/well/api/amazon/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.well.api.amazon.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.well.api.amazon.model.entities.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
