d024ade30cd519e3cc6e884cb215b53d8a3139ff
spagnol10 <wellyntonspagnol10@gmail.com>
2023-07-15 10:45:48 -0300

    Refactor all files and folders

--- src/main/java/br/com/well/api/amazon/application/AmazonApplication.java
+++ src/main/java/br/com/well/api/amazon/application/AmazonApplication.java
@@ -1,4 +1,4 @@
-package br.com.well.api.amazon;
+package br.com.well.api.amazon.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
