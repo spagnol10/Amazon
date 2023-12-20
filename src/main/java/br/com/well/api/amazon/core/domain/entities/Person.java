da75ad6cbca2967dee8d38b58930aa1de0d52a50
Wellytnon.spagnol <wellyntonspagnol10@gmail.com>
2023-12-19 21:01:46 -0300

    refactor

---
+++ src/main/java/br/com/well/api/amazon/core/domain/entities/Person.java
@@ -0,0 +1,17 @@
+package br.com.well.api.amazon.core.domain.entities;
+
+public class Person {
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/main/java/br/com/well/api/amazon/presentation/controller/UserController.java
+++ src/main/java/br/com/well/api/amazon/presentation/controller/UserController.java
@@ -1,21 +1,14 @@
 package br.com.well.api.amazon.presentation.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import br.com.well.api.amazon.core.domain.entities.Person;
 import br.com.well.api.amazon.core.domain.entities.User;
 import br.com.well.api.amazon.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -49,5 +42,11 @@ public class UserController {
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<?> delete(@PathVariable Long id) {
         return service.delete(id);
+    }
+
+    @GetMapping("/response")
+    public String response() {
+        Person well = new Person("Wellington");
+        return well.getName();
     }
 }
